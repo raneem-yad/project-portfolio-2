396a44b13d88a178942234051d3f09dffebfa81c
raneem-yad <eng.raneem.yad@gmail.com>
2024-02-09 09:45:39 +0100

    adding run game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,12 +20,36 @@ document.addEventListener("DOMContentLoaded", function () {
 
     for (let button of buttons) {
         button.addEventListener("click", function () {
-            let gameType = this.getAttribute("data-type");
-            console.log(gameType);
-            // runGame(gameType);
+            let  userChoice= this.getAttribute("data-type");
+            console.log(userChoice);
+            runGame(userChoice);
         });
     }
 });
+
+function runGame(userChoice) {
+
+    let ChosseScreen = document.getElementById("choose");
+    let resultScreen = document.getElementById("result");
+    //Get the user choice
+    let leftSide = userChoice;
+
+    //Get the computer choice
+    let rightSide= computerPick();
+
+    //Calculate and update the result
+    calculateWinnerRule(leftSide,rightSide);
+    // result.textContent = resultText;
+
+    //Go to the results screen
+    ChosseScreen.classList.remove("visible");
+    resultScreen.classList.add("visible");
+
+    //Show the icons results 
+    showIconsResult(leftSide,rightSide);
+
+
+}
 
 /**
  * Generates a random choice from a predefined set of options.
@@ -43,37 +67,99 @@ function computerPick() {
  * @param {string} rightSide The choice of the Computer.
  * @returns {void} Outputs the winner to the console.
  */
-function calculateWinnerRule(leftSide, rightSide){
-    if(leftSide === rightSide){
+function calculateWinnerRule(leftSide, rightSide) {
+    console.log(`at cal func left side is ${leftSide} and right side is ${rightSide}`);
+    if (leftSide === rightSide) {
         console.log("it's tie")
-    }else {
-        const rules  =[
-            {input: ["rock" ,"paper"], output: "paper"},
-            {input: ["rock" ,"lizard"], output: "rock"},
-            {input: ["rock" ,"scissor"], output: "rock"},
-            {input: ["rock" ,"spock"], output: "spock"},
-            {input: ["lizard" ,"paper"], output: "lizard"},
-            {input: ["lizard" ,"scissor"], output: "scissor"},
-            {input: ["lizard" ,"spock"], output: "lizard"},
-            {input: ["paper" ,"scissor"], output: "scissor"},
-            {input: ["paper" ,"spock"], output: "paper"},
-            {input: ["scissor" ,"spock"], output: "spock"},
+    } else {
+        const rules = [
+            { input: ["rock", "paper"], output: "paper" },
+            { input: ["rock", "lizard"], output: "rock" },
+            { input: ["rock", "scissor"], output: "rock" },
+            { input: ["rock", "spock"], output: "spock" },
+            { input: ["lizard", "paper"], output: "lizard" },
+            { input: ["lizard", "scissor"], output: "scissor" },
+            { input: ["lizard", "spock"], output: "lizard" },
+            { input: ["paper", "scissor"], output: "scissor" },
+            { input: ["paper", "spock"], output: "paper" },
+            { input: ["scissor", "spock"], output: "spock" },
         ];
-    
-        for(let rule of rules){
-            if(rule.input.include(leftSide) && rule.input.include(rightSide)){
+
+        for (let rule of rules) {
+            console.log(rule.output);
+            if (rule.input.includes(leftSide) && rule.input.includes(rightSide)) {
+                
+                console.log(`at first if func ${leftSide} and right side is ${rightSide}`);
                 console.log(rule.output);
-                if(rule.output === leftSide) {
-                    console.log("the winner for this round is user")
+
+                if (rule.output === leftSide) {
+                    console.log("the winner for this round is user");
+                    incrementPlayerScore();
                 } else {
-                    console.log("the winner for this round is computer")
+                    console.log("the winner for this round is computer");
+                    incrementComputerScore();
                 }
-            }else {
-                console.error("thers is no accepted rule");
+                // when you find the winner just break no need to check the other rules
+                break;
+            } else {
+                console.log("Not this rule");
             }
         }
     }
-    
+
+}
+
+/**
+ * Increments the player's score by 1.
+ * 
+ * @function
+ * @name incrementPlayerScore
+ * @returns {void}
+ */
+function incrementPlayerScore() {
+
+    let oldScore = parseInt(document.getElementById("user-score").innerText);
+    document.getElementById("user-score").innerText = ++oldScore;
+
+}
+
+/**
+ * Increments the computer's score by 1.
+ * 
+ * @function
+ * @name incrementComputerScore
+ * @returns {void}
+ */
+function incrementComputerScore() {
+
+    let oldScore = parseInt(document.getElementById("computer-score").innerText);
+    document.getElementById("computer-score").innerText = ++oldScore;
+
+}
+
+function showIconsResult(leftSide,rightSide){
+    let userPickedResult = document.getElementById('user-pick-icon');
+    let computerpickedResult = document.getElementById('computer-pick-icon');
+    const rulesIcons = {
+        paper: `<button class="res-icons paper" data-type="paper"><i class="fa-solid fa-hand"></i></button>`,
+        rock : `<button class="res-icons rock" data-type="rock"><i class="fa-solid fa-hand-back-fist"></i></button>`,
+        scissor:`<button class="res-icons scissor" data-type="scissors"><i class="fa-solid fa-hand-scissors"></i></button>`,
+        spock: `<button class="res-icons spock" data-type="spock"><i class="fa-solid fa-hand-spock"></i></button>`,
+        lizard: `<button class="res-icons lizard" data-type="lizard"><i class="fa-solid fa-hand-lizard"></i></button>`,
+    }
+    for(let icon in rulesIcons){
+        if(rulesIcons.hasOwnProperty(leftSide)&&rulesIcons.hasOwnProperty(rightSide)){
+            console.log(`the key icon is ${icon} and the value for this key is ${rulesIcons[icon]}`);
+            if(icon == leftSide) {
+                console.log(`player picked icon ${icon}`);
+                userPickedResult.innerHTML=rulesIcons[icon];
+            }else if(icon == rightSide){
+                console.log(`computer picked icon ${icon}`);
+                userPickedResult.innerHTML=rulesIcons[icon];
+            }
+            
+        }
+    }
 }
 
 function getPlayerNameFromURL() {
